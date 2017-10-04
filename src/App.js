9fd402740bf2c274badc70c34130c309ae55668b
Darlan Junior <darlan.sbjr@gmail.com>
2017-10-04 16:45:41 -0300

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 
 import { Container, Header } from 'semantic-ui-react';
+import { Link, Route } from 'react-router-dom';
 import React, { Component } from 'react';
+
+const FirstPage = () => <Container>
+  <Header>Hello Semantic</Header>
+  <Link to="/router">Go To /router</Link>
+</Container>
+
+const HelloRouter = () => <Container>
+  <Header>Hello Router</Header>
+  <Link to="/">Go To /</Link>
+</Container>
 
 class App extends Component {
   render() {
-    return (
-      <Container>
-        <Header>Hello Semantic</Header>
-      </Container>
-    );
+    return [
+      <Route path="/" component={FirstPage} exact/>,
+      <Route path="/router" component={HelloRouter} />
+    ];
   }
 }
 
